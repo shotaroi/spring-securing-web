45f37d13c8d4f36d6947e7340de60670f6c1b988
shotaroi <58347058+shotaroi@users.noreply.github.com>
2025-01-27 11:03:23 +0100

    Create project

---
+++ spring-securing-web/src/main/java/com/shotaroi/springsecuringweb/SpringSecuringWebApplication.java
@@ -0,0 +1,13 @@
+package com.shotaroi.springsecuringweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecuringWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecuringWebApplication.class, args);
+    }
+
+}
---
+++ spring-securing-web/src/test/java/com/shotaroi/springsecuringweb/SpringSecuringWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shotaroi.springsecuringweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecuringWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
