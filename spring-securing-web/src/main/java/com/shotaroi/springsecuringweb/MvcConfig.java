88d60ce50c9cb0d296ba6a7cbf14ea87bd4bb40e
shotaroi <58347058+shotaroi@users.noreply.github.com>
2025-01-28 02:11:50 +0100

    Update MvcConfig.java

--- spring-securing-web/src/main/java/com/shotaroi/springsecuringweb/MvcConfig.java
+++ spring-securing-web/src/main/java/com/shotaroi/springsecuringweb/MvcConfig.java
@@ -1,4 +1,12 @@
 package com.shotaroi.springsecuringweb;
 
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+
 public class MvcConfig {
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/login").setViewName("login");
+    }
 }
